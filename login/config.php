07f8d99f50dba52a83a06415b2e76800c00f45ca
Dennis <okaludu001@gmail.com>
2025-10-18 13:25:18 +0100

    updated

--- login/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'shaffell');
-   define('DB_PASSWORD', '-X09Obt)uK3cE8');
-   define('DB_DATABASE', 'shaffell_sd');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
